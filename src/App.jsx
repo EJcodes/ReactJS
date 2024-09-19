8c8e7eada6423ddae81817b38055903117c9b6c9
EJcodes <ihmdemon.exe@gmail.com>
2024-09-19 15:11:03 -0400

    added a component named Examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,17 @@
-import { useState, Fragment} from 'react';
 import CoreConcepts from './components/CoreConcepts.jsx';
 import Header from "./components/Header/Header.jsx";
-import TabButton from "./components/TabButton.jsx";
-import {EXAMPLES} from './data.js';
+import Examples from './components/Examples.jsx';
+
+
 
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState();
-
-  
-
-  function handleClick(selectedButton) {
-    // selectButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);
-  }
-
-  let tabContent = <p>Please select a topic.</p>;
-
-  if (selectedTopic) {
-    tabContent = <div id="tab-content">
-    <h3>
-      {EXAMPLES[selectedTopic].title}
-    </h3>
-    <p >
-      {EXAMPLES[selectedTopic].description}
-    </p>
-    <pre>
-      <code>
-        {EXAMPLES[selectedTopic].code}
-      </code>
-    </pre>
-  </div>
-  }
-  //to do get more familar with conidtional data 
   return (
     <>
       <Header/> 
       <main>
         <CoreConcepts />
-          <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={ selectedTopic === 'components' } onClick = { () => handleClick('components')}>Components</TabButton>
-            <TabButton isSelected={ selectedTopic === 'jsx' } onClick = { () => handleClick('jsx') }>JSX</TabButton>
-            <TabButton isSelected={ selectedTopic === 'props' }onClick = { () => handleClick('props') }>Props</TabButton>
-            <TabButton isSelected={ selectedTopic === 'state' }onClick = { () => handleClick('state') }>State</TabButton>
-          </menu>
-        {tabContent}
-        </section>
+        <Examples/>
       </main>
     </>
   );
